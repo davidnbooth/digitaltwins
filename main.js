58c7e957674ea7003fb2f01c8896865b130a068b
davidbooth <davidnbooth@gmail.com>
2021-07-14 20:22:43 -0400

    new pictures

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ const context = {
             material: "6105-T5 Aluminum",
             finish: "Anodized",
             color: "Silver",
-            img_src: "components/extrusion/extrusion.png",
+            img_src: "images/extrusion.png",
             ref_url: "https://www.mcmaster.com/47065T103",
-            cad_file: "components/extrusion/extrusion.sldprt",
-            fea_file: "components/extrusion/extrusion.script"
+            cad_file: "",
+            fea_file: ""
         },
         {
             title: "Short Corner Bracket",
@@ -20,10 +20,10 @@ const context = {
             material: "Anodized Aluminum",
             finish: "Anodized",
             color: "Silver",
-            img_src: "components/short_bracket/short_bracket.png",
+            img_src: "images/short_corner.png",
             ref_url: "https://www.mcmaster.com/47065T845/",
-            cad_file: "components/short_bracket/short_bracket.sldprt",
-            fea_file: "components/short_bracket/short_bracket.script"
+            cad_file: "",
+            fea_file: ""
         },
         {
             title: "Extended Corner Bracket",
@@ -31,10 +31,21 @@ const context = {
             material: "Anodized Aluminum",
             finish: "Anodized",
             color: "Silver",
-            img_src: "components/extended_bracket/extended_bracket.png",
+            img_src: "images/long_corner.png",
             ref_url: "https://www.mcmaster.com/47065T241/",
-            cad_file: "components/extended_bracket/extended_bracket.sldprt",
-            fea_file: "components/extended_bracket/extended_bracket.script"
+            cad_file: "",
+            fea_file: ""
+        },
+        {
+            title: "Small Tee",
+            description: ' for 1-1/2" High Rail T-Slotted Framing',
+            material: "Anodized Aluminum",
+            finish: "Anodized",
+            color: "Silver",
+            img_src: "images/tee.png",
+            ref_url: "https://www.mcmaster.com/47065T279/",
+            cad_file: "",
+            fea_file: ""
         },
         {
             title: "90 deg. Surface Bracket",
@@ -42,10 +53,21 @@ const context = {
             material: "Anodized Aluminum",
             finish: "Anodized",
             color: "Silver",
-            img_src: "components/90deg_surface/90deg_surface.png",
+            img_src: "images/surface_90.png",
             ref_url: "https://www.mcmaster.com/47065T271/",
-            cad_file: "components/90deg_surface/90deg_surface.sldprt",
-            fea_file: "components/90deg_surface/90deg_surface.script"
+            cad_file: "",
+            fea_file: ""
+        },
+        {
+            title: "90 deg. Flush Surface Bracket",
+            description: 'Sliver Flush 90 Degree Angle Bracket for 1.5" High Rail',
+            material: "Anodized Aluminum",
+            finish: "Anodized",
+            color: "Silver",
+            img_src: "images/flush_90.png",
+            ref_url: "https://www.mcmaster.com/3136N158/",
+            cad_file: "",
+            fea_file: ""
         },
         {
             title: "45 deg. Surface Bracket",
@@ -53,10 +75,10 @@ const context = {
             material: "Anodized Aluminum",
             finish: "Anodized",
             color: "Silver",
-            img_src: "components/45deg_surface/45deg_surface.png",
+            img_src: "images/surface_45.png",
             ref_url: "https://www.mcmaster.com/47065T309/",
-            cad_file: "components/45deg_surface/45deg_surface.sldprt",
-            fea_file: "components/45deg_surface/45deg_surface.script"
+            cad_file: "",
+            fea_file: ""
         },
         {
             title: "30 deg. Surface Bracket",
@@ -64,10 +86,10 @@ const context = {
             material: "Anodized Aluminum",
             finish: "Anodized",
             color: "Silver",
-            img_src: "components/30deg_surface/30deg_surface.png",
+            img_src: "images/surface_30.png",
             ref_url: "https://www.mcmaster.com/47065T32/",
-            cad_file: "components/30deg_surface/30deg_surface.sldprt",
-            fea_file: "components/30deg_surface/30deg_surface.script"
+            cad_file: "",
+            fea_file: ""
         },
         {
             title: "60 deg. Surface Bracket",
@@ -75,10 +97,10 @@ const context = {
             material: "Anodized Aluminum",
             finish: "Anodized",
             color: "Silver",
-            img_src: "components/60deg_surface/60deg_surface.png",
+            img_src: "images/surface_60.png",
             ref_url: "https://www.mcmaster.com/47065T366/",
-            cad_file: "components/60deg_surface/60deg_surface.sldprt",
-            fea_file: "components/60deg_surface/60deg_surface.script"
+            cad_file: "",
+            fea_file: ""
         },
     ]
 };
